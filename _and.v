b5c755bd5cdbf9a778dd204c7ead9a123b68ac04
alejandramagadan1710 <alejandra.magadan4337@alumnos.udg.mx>
2025-08-20 15:35:24 -0600

    Add files via upload

---
+++ _and.v
@@ -0,0 +1,11 @@
+//1.Definicion del modulo y su I/O
+//Dentro del parentesis se define el I/O
+
+module _and (input a , input b , output c);
+//2.Definicion de cables o componentes 
+//3.Asignaciones , Instancias y conexiones 
+
+assign c = a & b;
+
+endmodule
+
